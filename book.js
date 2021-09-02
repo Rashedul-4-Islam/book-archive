1cab754d46adfb2bf2bc91442ac719f30ba90d20
Rashedul Islame <rasheduzzaman521@gmail.comm>
2021-09-02 16:02:39 +0600

    latest

--- book.js
+++ book.js
@@ -1,66 +1,67 @@
 const errorMessage = document.getElementById('error-message').style.display = 'none';
 
+  /*============ search book =================*/
+
 const getSearchValue = () =>{
     const inputField = document.getElementById('input-field');
     const inputCheck = inputField.value
     inputField.value = '';
     if(inputCheck === ''){
-         displayError()
+      displayError();
     }else{
+      document.getElementById('error-message').style.display = 'none';
       fetch(`https://openlibrary.org/search.json?q=${inputCheck}`)
       .then(res => res.json())
-      .then(data => bookImage(data))
-    }
-   
-}
+      .then(data => bookDetails(data))
+    } 
+}  
 
+       /*============ error show  =================*/
 const displayError = () =>{
   document.getElementById('error-message').style.display = 'block';
 }
 
-const bookImage = books =>{
-      console.log(books);
+const bookDetails = books =>{
+      // console.log(books);
       const bookList = Object.values(books);
       const bookData = bookList[3];
 
-      /*============ Search the total books================*/
+      /*============ operation for total books================*/
       const bookNum = bookList[0];
       const informationField = document.getElementById('information');
       informationField.textContent = "";
       const h3 = document.createElement('h3');
       h3.innerText = `Total Book Found: ${bookNum}`;
-      const responsePart = document.getElementById('response-part');
-      responsePart.textContent = '';
+      const showDetails = document.getElementById('show-details');
+      showDetails.textContent = '';
       informationField.appendChild(h3)
-      // console.log(bookData);
-      bookData.forEach(book => {
-          // const demoFive = `${book.title}`;
-          // // console.log(demoFive);
+             /*============  condition check for validity =================*/
+      let emptyArray = books.docs[0];
+      if(emptyArray === undefined){
+            displayError();
+      }else{
+        bookData.forEach(book => {
          
-            
             const div = document.createElement('div');
             div.innerHTML = `
-            <div class="card border border-dark cardShadow" style="width: 18rem;">
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top " style="height:300px" alt="...">
+            <div class="card border border-dark cardShadow" style="width: 22rem; height:510px;">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top " style="height:300px" alt="...">
             <div class="card-body">
-              <h5 class="card-title">Book-Name: ${book.title}</h5>
+              <h5 class="card-title"><span class="fw-bold text-warning">Book-Name :</span> ${book.title}</h5>
              
             </div>
             <ul class="list-group list-group-flush">
-            <li class="list-group-item fw-bold">Author-Name: ${book.author_name[0]}</li>
-              <li class="list-group-item">Publisher-Name: ${book.publisher[0]}</li>
-              <li class="list-group-item">Publish-Date: ${book.publish_date[0]}</li> 
+            <li class="list-group-item fw-bold"><span class="fw-bold text-danger">Author-Name :</span> ${book.author_name}</li>
+              <li class="list-group-item"><span class="fw-bold text-primary">Publisher-Name</span>: ${book.publisher}</li>
+              <li class="list-group-item"><span class="fw-bold text-secondary">Publisher-Date</span>: ${book.publish_date}</li> 
             </ul>
           </div>
           `;
-            // console.log(div);
-            responsePart.appendChild(div);
-          
-         
-      })
+            showDetails.appendChild(div);
+          })
+      }
+      
+      
+     
       
 }
-
-
-// <li class="list-group-item">Language: ${book.language}</li>
- // <h6 class="card-text"></h6>
