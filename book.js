25ed6c53c2bcb155d2c0641aa5e1903e1840c5fc
Rashedul Islame <rasheduzzaman521@gmail.comm>
2021-09-02 09:24:17 +0600

    second-commit

--- book.js
+++ book.js
@@ -1,25 +1,32 @@
 const getSearchValue = () =>{
     const inputField = document.getElementById('input-field').value;
     inputField.textContent = "";
-    fetch(`https://openlibrary.org/search.json?q=${inputField}`)
+    fetch(`http://openlibrary.org/search.json?q=${inputField}`)
     .then(res => res.json())
-    .then(data => bookImage(data.docs))
+    .then(data => bookImage(data))
 }
 
 const bookImage = books =>{
-      // console.log(books);
-      const length = `${books.length}`;
-        console.log(length);
-        const informationField = document.getElementById('information');
-        informationField.textContent = "";
-        const h3 = document.createElement('h3');
-        h3.innerText = `
-              The Total search Result-${books.length}
-        `;
+      console.log(books);
+      const bookList = Object.values(books);
+      const bookData = bookList[3];
 
-        informationField.appendChild(h3)
+      /*============ Search the total books================*/
+      const bookNum = bookList[0];
+      const informationField = document.getElementById('information');
+      informationField.textContent = "";
+      const h3 = document.createElement('h3');
+       h3.innerText = `Total Book Found: ${bookNum}`;
+      informationField.appendChild(h3)
       
-      books.forEach(book => {
+      // console.log(bookData);
+      
+     
+       
+      
+      bookData.forEach(book => {
+      
+
           // console.log(book);
           // 
         //   const url = ` `;
@@ -28,7 +35,7 @@ const bookImage = books =>{
             const div = document.createElement('div');
             div.innerHTML = `
             <div class="card border border-dark cardShadow" style="width: 18rem;height:25rem;">
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top " style="height:300px" alt="...">
+            <img src="http://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top " style="height:300px" alt="...">
             <div class="card-body">
               <h5 class="card-title">Book-Name: ${book.title}</h5>
               <h6 class="card-text">Author-Name: ${book.author_name}</h6>
