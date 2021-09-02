f02687cb13c83c034ebfb9819b98aeee70077eeb
Rashedul Islame <rasheduzzaman521@gmail.comm>
2021-09-02 09:38:57 +0600

    third-commit

--- book.js
+++ book.js
@@ -1,7 +1,7 @@
 const getSearchValue = () =>{
     const inputField = document.getElementById('input-field').value;
     inputField.textContent = "";
-    fetch(`http://openlibrary.org/search.json?q=${inputField}`)
+    fetch(`https://openlibrary.org/search.json?q=${inputField}`)
     .then(res => res.json())
     .then(data => bookImage(data))
 }
@@ -16,39 +16,25 @@ const bookImage = books =>{
       const informationField = document.getElementById('information');
       informationField.textContent = "";
       const h3 = document.createElement('h3');
-       h3.innerText = `Total Book Found: ${bookNum}`;
+      h3.innerText = `Total Book Found: ${bookNum}`;
       informationField.appendChild(h3)
-      
       // console.log(bookData);
-      
-     
-       
-      
       bookData.forEach(book => {
-      
-
-          // console.log(book);
-          // 
-        //   const url = ` `;
-        // //   console.log(url);
           let responsePart = document.getElementById('response-part');
             const div = document.createElement('div');
             div.innerHTML = `
             <div class="card border border-dark cardShadow" style="width: 18rem;height:25rem;">
-            <img src="http://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top " style="height:300px" alt="...">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top " style="height:300px" alt="...">
             <div class="card-body">
               <h5 class="card-title">Book-Name: ${book.title}</h5>
               <h6 class="card-text">Author-Name: ${book.author_name}</h6>
             </div>
             <ul class="list-group list-group-flush">
               <li class="list-group-item">Publisher-Name: ${book.publisher}</li>
-              <li class="list-group-item">Publish-Date: ${book.publish_date}</li>
-              <li class="list-group-item">Language: ${book.language}</li>
+              <li class="list-group-item">Publish-Date: ${book.publish_date}</li> 
             </ul>
-           
           </div>
-                  
-                 `;
+          `;
             // console.log(div);
             responsePart.appendChild(div);
       })
@@ -56,4 +42,4 @@ const bookImage = books =>{
 }
 
 
-//
+// <li class="list-group-item">Language: ${book.language}</li>
