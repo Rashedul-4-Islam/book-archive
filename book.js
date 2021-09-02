e0deb9dbe3852c4590ad540d73f9782fd53f0be6
Rashedul Islame <rasheduzzaman521@gmail.comm>
2021-09-02 12:11:54 +0600

    checking

--- book.js
+++ book.js
@@ -29,16 +29,18 @@ const bookImage = books =>{
       informationField.textContent = "";
       const h3 = document.createElement('h3');
       h3.innerText = `Total Book Found: ${bookNum}`;
+      const responsePart = document.getElementById('response-part');
+      responsePart.textContent = '';
       informationField.appendChild(h3)
       // console.log(bookData);
       bookData.forEach(book => {
-          const demoFive = `${book.title}`;
-          // console.log(demoFive);
+          // const demoFive = `${book.title}`;
+          // // console.log(demoFive);
          
-            let responsePart = document.getElementById('response-part');
+            
             const div = document.createElement('div');
             div.innerHTML = `
-            <div class="card border border-dark cardShadow" style="width: 18rem;height:25rem;">
+            <div class="card border border-dark cardShadow" style="width: 18rem;">
             <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top " style="height:300px" alt="...">
             <div class="card-body">
               <h5 class="card-title">Book-Name: ${book.title}</h5>
